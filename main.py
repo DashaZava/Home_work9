9d50512bc06f4047c416e1f3efa6f5fd51813983
DashaZava <112952295+DashaZava@users.noreply.github.com>
2023-08-28 22:13:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,82 +1,38 @@
-def bot_assistant():
-    try:
-        answer = input('==> ').lower().strip()
-        command, *args = answer.split(' ')
-        if answer in exit_answer:
-            return exit_answer[3].capitalize()
-        elif answer.startswith(OPERATIONS[0]):
-            new_contact = add_contact(args)
-            if new_contact:
-                return new_contact
-        elif answer.startswith(OPERATIONS[1]):
-            update_contact = change_contact(args)
-            return update_contact
-        elif answer.startswith(OPERATIONS[2]):
-            contact = show_contact(args)
-            return contact
-        elif answer.startswith(OPERATIONS[3]):
-            return ""
-        elif answer.startswith(OPERATIONS[4]):
-            book = show_phone_book(answer)
-            return book
-        else:
-            return "Sorry, don't know this command. Try again."
-    except KeyboardInterrupt:
-        return exit_answer[3].capitalize()
+def bot_assistant(answer):
+    command, *args = answer.split(' ')
+    handler = command_handlers.get(command)
+    if handler:
+        return handler(args)
+    else:
+        return "Sorry, don't know this command. Try again."
 
-
-def main():
-    while True:
-        response = bot_assistant()
-        print_answer(response)
-
-
-def print_answer(message):
-    print(message)
-
-
-def input_error(func):
-    def inner(s):
-        try:
-            return func(s)
-        except KeyError:
-            return 'Write correct value:'
-        except ValueError:
-            return 'Write correct value:'
-        except IndexError:
-            return 'Write correct value:'
-
-    return inner
-
-
-@input_error
 def add_contact(args):
-    phone_book.update({args[0]: args[1]})
+    if len(args) != 2:
+        return "Write correct value: name phone"
+    phone_book[args[0]] = args[1]
     return f'Contact > {args[0].capitalize()} has been added'
 
-
-@input_error
 def change_contact(args):
-    if phone_book.get(args[0]):
-        answer = input('new phone number =>> ')
-        phone_book.update({args[0]: answer})
+    if len(args) != 2:
+        return "Write correct value: name phone"
+    if args[0] in phone_book:
+        new_phone = input('New phone number =>> ')
+        phone_book[args[0]] = new_phone
         return f'Contact > {args[0].capitalize()} has been updated'
     else:
         return f"Sorry, {args[0].capitalize()} can't be found"
 
-
-@input_error
 def show_phone_book(_):
     phoneBook = ''
-    for k, v in phone_book.items():
-        phoneBook += '| {name}: {value}'.format(name=k, value=v)
+    for name, phone in phone_book.items():
+        phoneBook += f'| {name}: {phone}\n'
     return phoneBook
 
-
-@input_error
 def show_contact(args):
-    return f'{phone_book.get(args[0])}'
+    return phone_book.get(args[0], "Contact not found")
 
+def print_answer(txt='How can I help you?'):
+    print(txt)
 
 OPERATIONS = [
     'add',
@@ -88,6 +44,23 @@ OPERATIONS = [
 exit_answer = ['.', 'close', 'exit', 'good bye']
 phone_book = {}
 
+command_handlers = {
+    OPERATIONS[0]: add_contact,
+    OPERATIONS[1]: change_contact,
+    OPERATIONS[2]: show_contact,
+    OPERATIONS[3]: lambda _: "",  # Empty function
+    OPERATIONS[4]: show_phone_book
+}
+
+def main():
+    while True:
+        try:
+            answer = input('==> ').lower().strip()
+            response = bot_assistant(answer)
+            print_answer(response)
+        except KeyboardInterrupt:
+            print_answer(f"{exit_answer[3].capitalize()}!")
+            break
+
 if __name__ == '__main__':
     main()
-
