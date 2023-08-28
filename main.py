b35da38f00b8a2bb8ef566eb9b15974da4c4a218
DashaZava <112952295+DashaZava@users.noreply.github.com>
2023-08-28 22:02:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,38 @@
 def bot_assistant():
+    try:
+        answer = input('==> ').lower().strip()
+        command, *args = answer.split(' ')
+        if answer in exit_answer:
+            return exit_answer[3].capitalize()
+        elif answer.startswith(OPERATIONS[0]):
+            new_contact = add_contact(args)
+            if new_contact:
+                return new_contact
+        elif answer.startswith(OPERATIONS[1]):
+            update_contact = change_contact(args)
+            return update_contact
+        elif answer.startswith(OPERATIONS[2]):
+            contact = show_contact(args)
+            return contact
+        elif answer.startswith(OPERATIONS[3]):
+            return ""
+        elif answer.startswith(OPERATIONS[4]):
+            book = show_phone_book(answer)
+            return book
+        else:
+            return "Sorry, don't know this command. Try again."
+    except KeyboardInterrupt:
+        return exit_answer[3].capitalize()
+
+
+def main():
     while True:
-        try:
-            answer = input('==> ').lower().strip()
-            command, *args = answer.split(' ')
-            if answer in exit_answer:
-                return print_answer(f"{exit_answer[3].capitalize()}!")
-            elif answer.startswith(OPERATIONS[0]):
-                new_contact = add_contact(args)
-                if new_contact:
-                    print_answer(new_contact)
-                else:
-                    pass
-            elif answer.startswith(OPERATIONS[1]):
-                update_contact = change_contact(args)
-                print_answer(update_contact)
-            elif answer.startswith(OPERATIONS[2]):
-                contact = show_contact(args)
-                print_answer(contact)
-            elif answer.startswith(OPERATIONS[3]):
-                print_answer()
-            elif answer.startswith(OPERATIONS[4]):
-                book = show_phone_book(answer)
-                print_answer(book)
-            else:
-                print_answer("Sorry, don't know this command. Try again.")
-        except KeyboardInterrupt:
-            return print_answer(f"{exit_answer[3].capitalize()}!")
+        response = bot_assistant()
+        print_answer(response)
+
+
+def print_answer(message):
+    print(message)
 
 
 def input_error(func):
@@ -33,11 +40,11 @@ def input_error(func):
         try:
             return func(s)
         except KeyError:
-            print_answer('Write correct value:')
+            return 'Write correct value:'
         except ValueError:
-            print_answer('Write correct value:')
+            return 'Write correct value:'
         except IndexError:
-            print_answer('Write correct value:')
+            return 'Write correct value:'
 
     return inner
 
@@ -53,9 +60,9 @@ def change_contact(args):
     if phone_book.get(args[0]):
         answer = input('new phone number =>> ')
         phone_book.update({args[0]: answer})
-        return f'Contact > {args[0].capitalize()} has been update'
+        return f'Contact > {args[0].capitalize()} has been updated'
     else:
-        return f"Sorry,{args[0].capitalize()} can't find"
+        return f"Sorry, {args[0].capitalize()} can't be found"
 
 
 @input_error
@@ -63,7 +70,6 @@ def show_phone_book(_):
     phoneBook = ''
     for k, v in phone_book.items():
         phoneBook += '| {name}: {value}'.format(name=k, value=v)
-
     return phoneBook
 
 
@@ -83,4 +89,5 @@ exit_answer = ['.', 'close', 'exit', 'good bye']
 phone_book = {}
 
 if __name__ == '__main__':
-    bot_assistant()
+    main()
+
